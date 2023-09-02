860961968638760ed5b273270635fdf215b37131
Alberto Pérez <63558530+k3ssdev@users.noreply.github.com>
2023-09-02 18:19:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Copiar fichero .zshrc, .p10k.zsh y kitty.conf del repositorio en las carpetas correspondientes
-cp .zshrc ~/
-cp .p10k.zsh ~/
-cp kitty.conf ~/.config/kitty/
-cp current-theme.conf ~/.config/kitty/
-
-
 # Instalar Kitty como terminal y configurarlo como predeterminado
 sudo apt-get update
 sudo apt-get install kitty -y
-sudo update-alternatives --config x-terminal-emulator
+#sudo update-alternatives --config x-terminal-emulator
 
 # Instalar powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
@@ -18,7 +11,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # Instalar themes de Kitty en ~/.config/kitty/
 mkdir -p ~/.config/kitty
-
 git clone https://github.com/kovidgoyal/kitty-themes.git ~/.config/kitty/
 
 # Instalar colorscripts
@@ -31,13 +23,24 @@ gem install colorls
 sudo apt-get install lsd -y
 sudo apt-get install betterlockscreen -y
 
+# Instalar fuentes Meslo
+mkdir -p ~/.fonts
+cp "MesloLGS NF Bold Italic.ttf" ~/.fonts
+cp "MesloLGS NF Bold.ttf" ~/.fonts
+cp "MesloLGS NF Italic.ttf" ~/.fonts
+cp "MesloLGS NF Regular.ttf" ~/.fonts
+
+# Añade fuentes de usuario (en ~/.fonts)
+fc-cache -f -v         
+
+
 # Descargar las fuentes MesloLGS en ~/Downloads
-mkdir -p ~/Downloads
-cd ~/Downloads
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+#mkdir -p ~/Downloads
+#cd ~/Downloads
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+#wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
 # Reiniciar la terminal para aplicar los cambios
 exec zsh
